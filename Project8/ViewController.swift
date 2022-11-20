3d38e790ac600f0d1dac8c9028c6361fe8d99212
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-11-20 17:00:10 +0300

    add GCD

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -236,45 +236,51 @@ class ViewController: UIViewController {
     }
     
     func loadLevel() {
-        var clueString = ""
-        var solutionString = ""
-        var letterBits = [String]()
         
-        if let levelFileURL = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") {
-            if let levelContents = try? String(contentsOf: levelFileURL) {
-                var lines = levelContents.components(separatedBy: "\n")
-                lines.shuffle()
+        DispatchQueue.global().async { [weak self] in
+            var clueString = ""
+            var solutionString = ""
+            var letterBits = [String]()
+            
+            if let level = self?.level,
+               let levelFileURL = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") {
+                if let levelContents = try? String(contentsOf: levelFileURL) {
+                    var lines = levelContents.components(separatedBy: "\n")
+                    lines.shuffle()
+                    
+                    for (index, line) in lines.enumerated() {
+                        let parts = line.components(separatedBy: ": ")
+                        
+                        let answer = parts[0]
+                        let clue = parts[1]
+                        
+                        clueString += "\(index + 1). \(clue)\n"
+                        
+                        let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                        solutionString += "\(solutionWord.count) letters\n"
+                        self?.solutions.append(solutionWord)
+                        
+                        let bits = answer.components(separatedBy: "|")
+                        letterBits += bits
+                    }
+                }
+            }
+           
+            DispatchQueue.main.async { [weak self] in
+                // Now configure the buttons and labels
+                self?.cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+                self?.answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
                 
-                for (index, line) in lines.enumerated() {
-                    let parts = line.components(separatedBy: ": ")
-                    
-                    let answer = parts[0]
-                    let clue = parts[1]
-                    
-                    clueString += "\(index + 1). \(clue)\n"
-                    
-                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
-                    solutionString += "\(solutionWord.count) letters\n"
-                    solutions.append(solutionWord)
-                    
-                    let bits = answer.components(separatedBy: "|")
-                    letterBits += bits
+                letterBits.shuffle()
+                
+                if let letterButtons = self?.letterButtons,
+                   letterBits.count == letterButtons.count {
+                    for i in 0 ..< letterButtons.count {
+                        letterButtons[i].setTitle(letterBits[i], for: .normal)
+                    }
                 }
             }
         }
-        
-        // Now configure the buttons and labels
-        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
-        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
-
-        letterBits.shuffle()
-
-        if letterBits.count == letterButtons.count {
-            for i in 0 ..< letterButtons.count {
-                letterButtons[i].setTitle(letterBits[i], for: .normal)
-            }
-        }
     }
-
 }
 
